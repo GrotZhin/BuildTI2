a2d606274b42ccb6f1344a213a027e1906d38073
GrotZhin <gabrielcamposlol20@gmail.com>
2025-05-12 23:35:17 -0300

    prot1.0.7
    parallax resolved

--- Prototipo/Assets/Script/Scene/Parallax.cs
+++ Prototipo/Assets/Script/Scene/Parallax.cs
@@ -22,14 +22,19 @@ public class Parallax : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
+        Vector2 pos = transform.position; 
+        Vector2 playerPos = player.transform.position; 
         cameraHalfSize = Camera.main.orthographicSize * Camera.main.aspect;
         screenLeft =  Camera.main.transform.position.x - cameraHalfSize;
         screenRight =  Camera.main.transform.position.x + cameraHalfSize;
 
         float realSpeed = player.speed.x / depth;
+        if (screenLeft >= pos.x + 4)
+        {
+            pos.x = playerPos.x + 19.2f;
+            
+        }
        
-        Vector2 pos = transform.position; 
         pos.x -= realSpeed * Time.fixedDeltaTime;
      
         transform.position = pos;
