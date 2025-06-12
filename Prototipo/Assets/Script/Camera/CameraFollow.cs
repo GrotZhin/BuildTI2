25cee80cc07028f75eead3ea8d5421753e6fdb18
Reg-Meme <michaeljacksonnothenoite@gmail.com>
2025-06-12 09:10:48 -0300

    Alpha1.2.7
    Cam

--- Prototipo/Assets/Script/Camera/CameraFollow.cs
+++ Prototipo/Assets/Script/Camera/CameraFollow.cs
@@ -30,7 +30,7 @@ public class CameraFollow : MonoBehaviour
             {
                 Vector3 pos = transform.position;
                 Vector2 playerPos = player.transform.position;
-                pos.x = playerPos.x;
+                pos.x = playerPos.x+2;
                 pos.y = playerPos.y;
 
                 transform.position = pos;
